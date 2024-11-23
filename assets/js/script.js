767c97957a21a4b5821cbbeccd8363f636d8d764
Augusto Brandão <gutoo.brandao@gmail.com>
2024-11-23 11:00:12 -0300

    adiciona o resto do conteúdo do site

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,11 @@
 "use strict";
 
-// Funtions
 function responsiveMenu() {
   const header = document.querySelector("header");
   const btnOpen = document.querySelector("#btn--menu-open");
   const btnClose = document.querySelector("#btn--menu-close");
 
   header.addEventListener("click", (e) => {
-    e.preventDefault();
-
     if (
       e.target.classList.contains("menu--icon") ||
       e.target.classList.contains("nav--link") ||
